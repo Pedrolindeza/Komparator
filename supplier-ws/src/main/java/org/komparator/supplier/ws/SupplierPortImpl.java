8151880cffe26dae14697874336d97b8acc04149
Pedro Lindeza <pedro.lindeza@tecnico.ulisboa.pt>
2017-03-23 20:32:06 +0000

    setups

--- supplier-ws/src/main/java/org/komparator/supplier/ws/SupplierPortImpl.java
+++ supplier-ws/src/main/java/org/komparator/supplier/ws/SupplierPortImpl.java
@@ -10,26 +10,26 @@ import org.komparator.supplier.domain.Purchase;
 import org.komparator.supplier.domain.QuantityException;
 import org.komparator.supplier.domain.Supplier;
 
-// TODO
-//@WebService(
-//		endpointInterface = "org.komparator.supplier.ws.SupplierPortType", 
-//		wsdlLocation = "...", 
-//		name = "SupplierWebService", 
-//		portName = "...Port", 
-//		targetNamespace = "...", 
-//		serviceName = "...Service"
-//)
-public class SupplierPortImpl { // implements SupplierPortType {
+@SuppressWarnings("unused")
+@WebService(
+		endpointInterface = "org.komparator.supplier.ws.SupplierPortType", 
+		wsdlLocation = "supplier.1_0.wsdl", 
+		name = "SupplierWebService", 
+		portName = "SupplierPort", 
+		targetNamespace = "http://ws.supplier.komparator.org/", 
+		serviceName = "SupplierService")
+
+public class SupplierPortImpl implements SupplierPortType {
 
 	// end point manager
 	private SupplierEndpointManager endpointManager;
-
+	
 	public SupplierPortImpl(SupplierEndpointManager endpointManager) {
 		this.endpointManager = endpointManager;
 	}
 
 	// Main operations -------------------------------------------------------
-
+	@Override
 	public ProductView getProduct(String productId) throws BadProductId_Exception {
 		// check product id
 		if (productId == null)
@@ -49,7 +49,7 @@ public class SupplierPortImpl { // implements SupplierPortType {
 		// product not found
 		return null;
 	}
-
+	@Override
 	public List<ProductView> searchProducts(String descText) throws BadText_Exception {
 		// TODO
 		
@@ -59,6 +59,7 @@ public class SupplierPortImpl { // implements SupplierPortType {
 		return null;
 	}
 
+	@Override
 	public String buyProduct(String productId, int quantity)
 			throws BadProductId_Exception, BadQuantity_Exception, InsufficientQuantity_Exception {
 		// TODO
@@ -71,6 +72,7 @@ public class SupplierPortImpl { // implements SupplierPortType {
 
 	// Auxiliary operations --------------------------------------------------
 
+	@Override
 	public String ping(String name) {
 		if (name == null || name.trim().length() == 0)
 			name = "friend";
@@ -82,11 +84,11 @@ public class SupplierPortImpl { // implements SupplierPortType {
 		builder.append(" from ").append(wsName);
 		return builder.toString();
 	}
-
+	@Override
 	public void clear() {
 		Supplier.getInstance().reset();
 	}
-
+	@Override
 	public void createProduct(ProductView productToCreate) throws BadProductId_Exception, BadProduct_Exception {
 		// check null
 		if (productToCreate == null)
@@ -115,7 +117,7 @@ public class SupplierPortImpl { // implements SupplierPortType {
 		Supplier s = Supplier.getInstance();
 		s.registerProduct(productId, productDesc, quantity, price);
 	}
-
+	@Override
 	public List<ProductView> listProducts() {
 		Supplier supplier = Supplier.getInstance();
 		List<ProductView> pvs = new ArrayList<ProductView>();
@@ -126,7 +128,7 @@ public class SupplierPortImpl { // implements SupplierPortType {
 		}
 		return pvs;
 	}
-
+	@Override
 	public List<PurchaseView> listPurchases() {
 		Supplier supplier = Supplier.getInstance();
 		List<PurchaseView> pvs = new ArrayList<PurchaseView>();
@@ -175,6 +177,7 @@ public class SupplierPortImpl { // implements SupplierPortType {
 	}
 
 	/** Helper method to throw new BadText exception */
+	@SuppressWarnings("unused")
 	private void throwBadText(final String message) throws BadText_Exception {
 		BadText faultInfo = new BadText();
 		faultInfo.message = message;
@@ -182,6 +185,7 @@ public class SupplierPortImpl { // implements SupplierPortType {
 	}
 
 	/** Helper method to throw new BadQuantity exception */
+	@SuppressWarnings("unused")
 	private void throwBadQuantity(final String message) throws BadQuantity_Exception {
 		BadQuantity faultInfo = new BadQuantity();
 		faultInfo.message = message;
@@ -189,6 +193,7 @@ public class SupplierPortImpl { // implements SupplierPortType {
 	}
 
 	/** Helper method to throw new InsufficientQuantity exception */
+	@SuppressWarnings("unused")
 	private void throwInsufficientQuantity(final String message) throws InsufficientQuantity_Exception {
 		InsufficientQuantity faultInfo = new InsufficientQuantity();
 		faultInfo.message = message;
