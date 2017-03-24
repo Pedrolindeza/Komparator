1bbf979400c9f5e6578aa51a7a154a30ae029ad3
FilipeAz <filipepazevedo@gmail.com>
2017-03-24 00:38:23 +0000

    testes feitos

--- supplier-ws-cli/src/test/java/org/komparator/supplier/ws/it/SearchProductsIT.java
+++ supplier-ws-cli/src/test/java/org/komparator/supplier/ws/it/SearchProductsIT.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.After;
@@ -16,17 +17,59 @@ import org.komparator.supplier.ws.*;
 /**
  * Test suite
  */
+@SuppressWarnings("unused")
 public class SearchProductsIT extends BaseIT {
 
 	// static members
 
 	// one-time initialization and clean-up
 	@BeforeClass
-	public static void oneTimeSetUp() {
+	public static void oneTimeSetUp() throws BadProductId_Exception, BadProduct_Exception{
+		// clear remote service state before all tests
+		client.clear();
+
+		// fill-in test products
+		// (since getProduct is read-only the initialization below
+		// can be done once for all tests in this suite)
+		{
+			ProductView product = new ProductView();
+			product.setId("X1");
+			product.setDesc("Basketball");
+			product.setPrice(10);
+			product.setQuantity(10);
+			client.createProduct(product);
+		}
+		{
+			ProductView product = new ProductView();
+			product.setId("Y2");
+			product.setDesc("Baseball");
+			product.setPrice(20);
+			product.setQuantity(20);
+			client.createProduct(product);
+		}
+		{
+			ProductView product = new ProductView();
+			product.setId("Z3");
+			product.setDesc("Soccer ball");
+			product.setPrice(30);
+			product.setQuantity(30);
+			client.createProduct(product);
+		}
+		{
+			ProductView product = new ProductView();
+			product.setId("Y3");
+			product.setDesc("Baseball");
+			product.setPrice(40);
+			product.setQuantity(40);
+			client.createProduct(product);
+		}
+		
 	}
 
 	@AfterClass
 	public static void oneTimeTearDown() {
+		// clear remote service state after all tests
+		client.clear();
 	}
 
 	// members
@@ -48,11 +91,64 @@ public class SearchProductsIT extends BaseIT {
 
 	// bad input tests
 
-	// TODO
+	@Test(expected = BadText_Exception.class)
+	public void searchProductsNullTest() throws BadText_Exception {
+		client.searchProducts(null);
+	}
 
+	@Test(expected = BadText_Exception.class)
+	public void searchProductsEmptyTest() throws BadText_Exception {
+		client.searchProducts("");
+	}
+
+	@Test(expected = BadText_Exception.class)
+	public void searchProductsWhitespaceTest() throws BadText_Exception {
+		client.searchProducts(" ");
+	}
+
+	@Test(expected = BadText_Exception.class)
+	public void searchProductsTabTest() throws BadText_Exception {
+		client.searchProducts("\t");
+	}
+
+	@Test(expected = BadText_Exception.class)
+	public void searchProductsNewlineTest() throws BadText_Exception {
+		client.searchProducts("\n");
+	}
 	
 	// main tests
 
-	// TODO
+	@Test
+	public void searchProductsDescription() throws BadText_Exception {
+		List<ProductView> l = client.searchProducts("Baseball");
+		
+		for(ProductView pv : l) {
+			assertEquals("Baseball", pv.getDesc());
+		}
+	}
+	
+	@Test
+	public void searchProductsNormal() throws BadText_Exception {
+		List<ProductView> l = client.searchProducts("Baseball");
+		List<ProductView> baseballList = new ArrayList<ProductView>();
+		ProductView product = new ProductView();
+		product.setId("Y2");
+		product.setDesc("Baseball");
+		product.setPrice(20);
+		product.setQuantity(20);
+
+		baseballList.add(product);
+		
+		product.setId("Y3");
+		product.setDesc("Baseball");
+		product.setPrice(40);
+		product.setQuantity(40);
+
+		baseballList.add(product);
+		
+		for(ProductView pv : l) {
+			assertTrue(baseballList.contains(pv));
+		}
+	}
 
 }
