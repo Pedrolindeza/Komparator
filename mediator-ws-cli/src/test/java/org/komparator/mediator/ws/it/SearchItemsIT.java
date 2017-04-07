23a24647027cc2dc5236bb2df6eda17934951f1a
Pedro Lindeza <pedro.lindeza@tecnico.ulisboa.pt>
2017-04-07 23:35:00 +0100

    SearchItemsIT

--- mediator-ws-cli/src/test/java/org/komparator/mediator/ws/it/SearchItemsIT.java
+++ mediator-ws-cli/src/test/java/org/komparator/mediator/ws/it/SearchItemsIT.java
@@ -1,5 +1,33 @@
 package org.komparator.mediator.ws.it;
 
-public class SearchItemsIT extends BaseIT {
+import org.junit.Test;
+import org.komparator.mediator.ws.InvalidText_Exception;
 
+public class SearchItemsIT extends BaseIT {
+ 
+  @Test(expected = InvalidText_Exception.class)
+  public void nullId() throws InvalidText_Exception{
+   mediatorClient.searchItems(null);
+  }
+  
+  @Test(expected = InvalidText_Exception.class)
+  public void emptyId() throws InvalidText_Exception{
+   mediatorClient.searchItems("");
+  }
+  
+  @Test(expected = InvalidText_Exception.class)
+  public void onlySpacesId() throws InvalidText_Exception{
+   mediatorClient.searchItems("                         ");
+  }
+  
+  @Test(expected = InvalidText_Exception.class)
+  public void tabId() throws InvalidText_Exception{
+   mediatorClient.searchItems("\t");
+  }
+  
+  @Test(expected = InvalidText_Exception.class)
+  public void newLineId() throws InvalidText_Exception{
+   mediatorClient.searchItems("\n");
+  }
+  
 }
